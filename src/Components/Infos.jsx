fcd40efdb8acedfae55775397fa2e9b5386e1685
Pedro Neto <netopedroo117@gmail.com>
2022-10-09 00:38:41 -0300

    editando commit

--- src/Components/Infos.jsx
+++ src/Components/Infos.jsx
@@ -1,17 +1,17 @@
 import styles from "../CSS/Infos.module.css";
-import icon from '../images/logo.png'
-function Infos() {
+
+function Infos(){
   return (
     <section className={styles.corpo}>
       <div className={styles.texto}>
-        <h1>Se informe contra Fake News</h1>
-        <p>Na Era da <span className={styles.span}>"pós verdade"</span>  somente com informações 
+        <h1>A importancia de votar</h1>
+        <p>Inserido em um realidade completa de ignorancia <span className={styles.span}>"é de extrema importância"</span> se manter atualizado
         <span className={styles.span}> verdadeiras</span>  vamos fazer uma política justa</p>
       </div>
       <div className={styles.container}>
         <div className={styles.quadro}>
-        <img src={icon} alt="provisorio" />
-
+        <img src="https://cdn-icons-png.flaticon.com/512/3099/3099186.png"alt="provisorio" className={styles.icones} />
+          <hr />
           <h3>Porque é importante votar</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
@@ -21,6 +21,8 @@ function Infos() {
           </p>
         </div>
         <div className={styles.quadro}>
+        <img src="https://cdn-icons-png.flaticon.com/512/5692/5692286.png" alt="provisorio" className={styles.icones} />
+        <hr />
           <h3>Porque é importante votar</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
@@ -30,6 +32,8 @@ function Infos() {
           </p>
         </div>
         <div className={styles.quadro}>
+        <img src="https://cdn-icons-png.flaticon.com/512/3080/3080467.png" alt="provisorio" className={styles.icones} />
+        <hr />
           <h3>Porque é importante votar</h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
