fb300b903e5f7542e44090c8a81fdb1f09fb6ab7
Pedro Neto <netopedroo117@gmail.com>
2022-10-08 22:23:16 -0300

    Estilizando o footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,15 +1,24 @@
 import { Link } from 'react-router-dom'
 import styles from '../CSS/Footer.module.css'
-import logo from '../images/logo.png'
+import logo from '../images/logoluan.png'
 function Footer(){
     return(
         <footer className={styles.rodape}>
+         <div className={styles.social}>
+            <a href="#" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/4138/4138124.png" alt="insta" className={styles.icones}/></a>
+            <a href="#" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/1384/1384053.png" alt="facebook" className={styles.icones}/></a>
+            <a href="#" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/5968/5968705.png" alt="figma" className={styles.icones}/></a>
+         </div>
+         <ol className={styles.list}>
+            <li>Home</li>
+            <li>Informativo</li>
+            <li>Candidatos</li>
+            <li>Recomendações</li>
+         </ol>
+         <hr />
+        <div className={styles.logo}>
          <img src={logo} alt="logo" />
-
-            <h1>texto</h1>
-         
-            
-
+        </div>
         </footer>
     )
 }
