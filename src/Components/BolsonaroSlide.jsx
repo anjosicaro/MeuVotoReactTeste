679b89280282b871b4f6d7312034b8b068d392eb
Pedro Neto <netopedroo117@gmail.com>
2022-10-11 23:27:45 -0300

    slide, grafico

--- src/Components/BolsonaroSlide.jsx
+++ src/Components/BolsonaroSlide.jsx
@@ -1,15 +1,50 @@
-import styles from '../CSS/BolsonaroSlide.module.css'
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function BolsonaroSlide(){
-    return(
-        <section className={styles.corpo}>
-            <h1>
-                Veja mais noticias sobre o candidato
-            </h1>
-            <div className={styles.Slide}>
-                slide
-            </div>
-        </section>
-    )
+function CarouselFadeExample({titulo, texto, imagens}) {
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={imagens}
+          alt="First slide"
+          width="100px"
+          height="100px"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={imagens}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={imagens}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
-export default BolsonaroSlide
+
+export default CarouselFadeExample;
--- src/Components/Lula.jsx
+++ src/Components/Lula.jsx
@@ -1,26 +1,48 @@
 import styles from "../CSS/Lula.module.css";
 import LulaFoto from "../images/Lula.png";
-import LulaHist from "./LulaHist";
-import LulaSlide from "./LulaSlide";
+import CarouselFadeExample from "./LulaSlide.jsx";
+import React from "react";
+import { Chart } from "react-google-charts";
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Work", 11],
+  ["Eat", 2],
+  ["Commute", 2],
+  ["Watch TV", 2],
+  ["Sleep", 7],
+];
+
+export const options = {
+  title: "My Daily Activities",
+  is3D: true,
+};
 
 function Lula() {
   return (
-  <>
-    <section className={styles.corpo}>
-      <div className={styles.Container}>
-        <div className={styles.fundoImagem}>
-          <img src={LulaFoto} alt="Foto Lula" />
+    <>
+      <section className={styles.corpo}>
+        <div className={styles.Container}>
+          <div className={styles.fundoImagem}>
+            <img src={LulaFoto} alt="Foto Lula" />
+          </div>
+          <div className={styles.Perfil}>
+            <h1>Luiz Inacio <span className={styles.span}>Lula</span> da Silva</h1>
+            <h4>Partido dos <span className={styles.span}>Trabalhadores - 13</span> </h4>
+            <p>Conheça a trajetoria politica <br /> do candidato e ex presidente </p>
+          </div>
+          <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+          />
         </div>
-        <div className={styles.Perfil}>
-          <h1>Luiz Inacio <span className={styles.span}>Lula</span> da Silva</h1>
-          <h4>Partido dos <span className={styles.span}>Trabalhadores - 13</span> </h4>
-          <p>Conheça a trajetoria politica <br /> do candidato e ex presidente </p>
-        </div>
-      </div>
-    </section>
-    <LulaHist />
-    <LulaSlide />
-  </>
+      </section>
+      {/* <LulaHist /> */}
+      <CarouselFadeExample />
+    </>
   );
 }
 export default Lula;
