13812f24aa3ced433bf1dc0d8d8b7e2eaf96c84d
Pedro Neto <netopedroo117@gmail.com>
2022-10-12 16:00:27 -0300

    Alterando botões dos candidatos

--- src/Components/Candidatos.jsx
+++ src/Components/Candidatos.jsx
@@ -10,23 +10,20 @@ function Candidatos() {
           Candidatos para o <span>segundo</span> turno
         </h1>
         <p>
-        Os candidatos Luiz Inácio Lula da Silva (PT) e Jair Messias Bolsonaro (PL) vão disputar o segundo turno para a Presidência da República nas Eleições 2022, Lula obteve 47,91% (55.166.851) e Bolsonaro 43,65% (50.261.216) dos votos analisados no primeiro turno (02/10/2022).
+          Os candidatos Luiz Inácio Lula da Silva (PT) e Jair Messias Bolsonaro (PL) vão disputar o segundo turno para a Presidência da República nas Eleições 2022, Lula obteve 47,91% (55.166.851) e Bolsonaro 43,65% (50.261.216) dos votos analisados no primeiro turno (02/10/2022).
         </p>
       </div>
       <div className={styles.Containercandidato}>
-      <div className={styles.esquerda}>
-        <img src={Lula} alt="Lula" className={styles.imgLula} />
-
-        <a href="/Lula" className={styles.botaoEsquerdo}>
-          Saiba mais
-        </a>
-      </div>
-      <div className={styles.direita}>
-        <img src={Bolsonaro} alt="Bolsonaro" className={styles.imgBolsonaro} />
-        <a href="/Bolsonaro" className={styles.botaoDireito}>
-          Saiba Mais
-        </a>
-      </div>
+        <div className={styles.esquerda}>
+          <img src={Lula} alt="Lula" className={styles.imgLula} />
+            <a href="/Lula" className={styles.esqBotao}>
+              Saiba mais!
+            </a>
+        </div>
+        <div className={styles.direita}>
+          <img src={Bolsonaro} alt="Bolsonaro" className={styles.imgBolsonaro} />
+          <a href="/Bolsonaro" className={styles.button}> Saiba mais!</a>
+        </div>
       </div>
 
     </section>
