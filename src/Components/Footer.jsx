66bd8b5a1aaf435d7524b39914fde5c504cb5b17
Jean3Lucas <1jean3lucas@gmail.com>
2022-10-13 04:37:30 -0300

    footer trash, ajuste home

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,42 +2,57 @@ import { Link } from 'react-router-dom'
 import styles from '../CSS/Footer.module.css'
 import logo from '../images/logo.png'
 
+
 function Footer() {
     return (
         <footer className={styles.rodape}>
             <div className={styles.container}>
-                <div className={styles.linha}>
-                    <div className={styles.colunas}>
+            <div className={styles.colunas}>
+                    <nav>
                         <h4>Sobre nós</h4>
-                        <nav>
                             <ul>
                                 <Link to='/'>
                                     <li>Página Inicial</li>
                                 </Link>
                                 <Link to="/Infos">
-                                    <li>Politica de Privacidade</li>
+                                    <li>Política de Privacidade</li>
                                 </Link>
                                 <Link to="/Recomendacao">
                                     <li>Parceiros</li>
                                 </Link>
                             </ul>
-
-                        </nav>
-                    </div>
+                     </nav>
+                </div>
+                <div className={styles.colunas}>
+                    <nav>
+                        <h4>Acessibilidade</h4>
+                            <ul>
+                                <Link to='/'>
+                                    <li>Modo Escuro</li>
+                                </Link>
+                                <Link to="/Infos">
+                                    <li>A++</li>
+                                </Link>
+                                <Link to="/Recomendacao">
+                                    <li>Daltonismo</li>
+                                </Link>
+                            </ul>
+                     </nav>
+                </div>
                     <div className={styles.colunas}>
-                        <h4>Acompanhe nos</h4>
+                        <h4>se atualiza:</h4>
                         <div className={styles.midias}>
                             <img src="https://cdn-icons-png.flaticon.com/512/739/739237.png" alt="fac" />
                             <img src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png" alt="inst" />
                             <img src="https://cdn-icons-png.flaticon.com/512/5968/5968704.png" alt="fig" />
                         </div>
                     </div>
-                    <div className={styles.colunas}>
+                    <div className={styles.colunass}>
                         <Link to="/">
-                            <img src={logo} alt="logo" />
+                            <img src={logo} alt="logo" id="logoo" />
                         </Link>
                     </div>
-                </div>
+                
             </div>
         </footer>
     )
