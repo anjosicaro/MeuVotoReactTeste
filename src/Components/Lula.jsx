54f66074988c528d2dc2495d4fabba2f6e3c45db
Jean3Lucas <1jean3lucas@gmail.com>
2022-10-13 05:22:21 -0300

    ajuste paginas candidatos

--- src/Components/Lula.jsx
+++ src/Components/Lula.jsx
@@ -30,7 +30,7 @@ function Lula() {
         </div>
         <div className={styles.Info}>
           <h1>Luiz Inacio <span className={styles.span}>Lula</span> da Silva</h1>
-          <h4>Partido dos <span className={styles.span}>Trabalhadores - 13</span></h4>
+          <h4>Partido dos Trabalhadores - <span className={styles.span}>13</span></h4>
           <p>Conheça a trajetoria politica <br /> do candidato e ex presidente </p>
           <p>O plano entregue pelo candidato afirma que ele tem compromisso com a defesa da igualdade, da democracia, da soberania e da paz. Também é compromisso de Lula buscar o desenvolvimento econômico sustentável, a justiça social, bem como reconhecer a diversidade. Com o objetivo de "recolocar os pobres e os trabalhores no orçamento", Lula pretende "revogar o teto de gastos e rever o atual regime fiscal brasileiro". O candidato chama o atual modelo de disfuncional e sem credibilidade. Lula propõe, ainda, fortalecer e modernizar a estrutura produtiva por meio da reindustrialização e do fortalecimento da produção agropecuária.</p>
           <a href="https://divulgacandcontas.tse.jus.br/candidaturas/oficial/2022/BR/BR/544/candidatos/893498/5_1659820284477.pdf" target="_blank" rel="noreferrer">Para mais informações</a>
